aae4a54a18a46dc83b0a716bf9968321a71d89aa
Jer Miller <jeremie.miller@gmail.com>
2025-02-17 14:52:07 -0700

    simple unsafe gnome shell extension to get background screenshots via dbus w/o screen flashes

---
+++ unsafe@persistent/README.md
@@ -0,0 +1,4 @@
+From parent directory:
+* `gnome-extensions pack unsafe@persistent`
+* `gnome-extensions install unsafe@persistent.shell-extension.zip`
+* Log out/in
---
+++ unsafe@persistent/extension.js
@@ -0,0 +1,10 @@
+function init() {
+}
+
+function enable() {
+    global.context.unsafe_mode = true;
+}
+
+function disable() {
+    global.context.unsafe_mode = false;
+}
